8049d902ee5f97485d8eb778f8be14d41e966ca3
Sovannet07 <novsovannet6@gmail.com>
2026-03-09 20:07:40 +0700

    finish

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -1,8 +1,8 @@
 ﻿using asp.net_api_teaching.Data;
 using asp.net_api_teaching.Request;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace asp.net_api_teaching.Controllers
 {
@@ -46,6 +46,7 @@ namespace asp.net_api_teaching.Controllers
         {
             try
             {
+                var jsonData = JsonConvert.SerializeObject(req.Items);
                 var parms = new SqlParameter[]
                 {
                     new SqlParameter("@InvoiceDate", req.InvoiceDate),
@@ -53,9 +54,7 @@ namespace asp.net_api_teaching.Controllers
                     new SqlParameter("@DiscountAmount", req.DiscountAmount),
                     new SqlParameter("@PaidAmount", req.PaidAmount),
                     new SqlParameter("@CustomerId", req.CustomerId),
-                    new SqlParameter("@ProductId", req.ProductId),
-                    new SqlParameter("@SaleQty", req.SaleQty),
-                    new SqlParameter("@UnitPrice", req.UnitPrice),
+                    new SqlParameter("@Items", jsonData),
                 };
                 var dt = DataManager.ExecuteSPReturnDt(_db, "[dbo].[SP_API_INVOICE_CREATE_V1]", parms);
                 var row = dt.Rows[0];
@@ -70,5 +69,33 @@ namespace asp.net_api_teaching.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, new { code = 1, message = ex.Message });
             }
         }
+
+        // get all invoices
+        [HttpPost("get-data-for-sale")]
+        public IActionResult GetDataForSale()
+        {
+            try
+            {
+                var ds = DataManager.ExtractDataSet(_db, "[dbo].[SP_API_INVOICE_GET_DATA_SALE_V1]");
+                var row = ds.Tables[0].Rows[0];
+                int code = Convert.ToInt32(row["code"]);
+                string message = row["message"].ToString()!;
+                if (code != 0)
+                {
+                    return BadRequest(new { code, message });
+                }
+                var customers = DataManager.ExtractDataTableToObjectList(ds.Tables[1]);
+                var products = DataManager.ExtractDataTableToObjectList(ds.Tables[2]);
+                return Ok(new { code, message, customers ,products});
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new { code = 1, message = ex.Message });
+            }
+        }
+
     }
 }
+
+
--- Request/InvoiceRequest.cs
+++ Request/InvoiceRequest.cs
@@ -7,9 +7,13 @@
         public decimal DiscountAmount { get; set; }
         public decimal PaidAmount { get; set; }
         public int CustomerId { get; set; }
+        public required List<ItemBinding> Items { get; set; }
+    }
+
+    public class ItemBinding
+    {
         public int ProductId { get; set; }
         public decimal SaleQty { get; set; }
         public decimal UnitPrice { get; set; }
-
     }
 }
